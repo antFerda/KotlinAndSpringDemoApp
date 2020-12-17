d6735d1b2451e6546e257bdc926513ef6fc5209e
Rustem <r.zhumagulov@gmail.com>
2020-12-17 17:32:45 +0600

    add detailed status

--- src/main/kotlin/ru/nse/alumniReunion2020/paymentService/DTO.kt
+++ src/main/kotlin/ru/nse/alumniReunion2020/paymentService/DTO.kt
@@ -35,7 +35,36 @@ data class PaymentStatus(
         val id: String,
         val status: String,
         val paid: Boolean,
-        val amount: Amount
+        val amount: Amount,
+        var cancellationDetails: CancellationDetails?
+)
+
+class CancellationDetails(
+        val party: String,
+        private val reason: String
+) {
+    val detailedReason = details[reason]
+}
+
+var details = mapOf(
+        Pair("3d_secure_failed", "Не пройдена аутентификация по 3-D Secure."),
+        Pair("call_issuer", "Оплата данным платежным средством отклонена по неизвестным причинам."),
+        Pair("canceled_by_merchant", "Платеж отменен получателем."),
+        Pair("card_expired", "Истек срок действия банковской карты."),
+        Pair("country_forbidden", "Вы не можете оплатить банковской картой из этой страны."),
+        Pair("expired_on_capture", "Истек срок списания оплаты."),
+        Pair("expired_on_confirmation", "Истек срок подтверждения оплаты."),
+        Pair("fraud_suspected", "Платеж заблокирован."),
+        Pair("general_decline", "Нет данных по детализации ошибки."),
+        Pair("identification_required", "Превышены ограничения на платежи для кошелька ЮMoney."),
+        Pair("insufficient_funds", "Недостаточно средств на счету."),
+        Pair("internal_timeout", "Технические неполадке на стороне платежного шлюза."),
+        Pair("invalid_card_number", "Неправильно указан номер карты."),
+        Pair("invalid_csc", "Неправильно указан код CVV2 (CVC2, CID). "),
+        Pair("issuer_unavailable", "Организация, выпустившая платежное средство, недоступна."),
+        Pair("payment_method_limit_exceeded", "Исчерпан лимит платежей для данного платежного средства"),
+        Pair("payment_method_restricted", "Операции данным платежным средством запрещены."),
+        Pair("permission_revoked", "Нельзя провести безакцептное списание: пользователь отозвал разрешение.")
 )
 
 data class CounterStatus(
